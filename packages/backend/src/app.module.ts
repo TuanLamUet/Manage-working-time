58ccd8ecc7f4eb83714229e17c065a5f18b8867e
Lâm Tuấn <tupikano1@gmail.com>
2020-05-16 21:36:25 +0700

    connect to database with typeorm

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
+  }), TypeOrmModule.forRoot({
+    autoLoadEntities: true,
   })],
   controllers: [AppController],
   providers: [AppService],
